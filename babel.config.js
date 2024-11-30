88f88f0ce57f2e199603abee1de204bf2ea8ff55
Janitha Amarasiri Jayasinghe <janithaaj@gmail.com>
2024-11-30 10:36:29 +0530

    feat: reade me file changed

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-undef, @typescript-eslint/no-require-imports */
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+};
---
+++ commitlint.config.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-undef, @typescript-eslint/no-require-imports */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
